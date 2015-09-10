e32678ca05457397021fa7939f1c4c30d5876f39
ChristopheMalo <christof.malo@gmail.com>
2015-09-10 14:47:40 +0200

    Initialiser la classe Personnage

---
+++ classes/Personnage.php
@@ -0,0 +1,19 @@
+<?php
+class Personnage {
+    private $_force;                        // La force du personnage
+    private $_localisation;                 // Sa localisation
+    private $_experience;                   // Son expérience
+    private $_degats;                       // Ses dégâts
+    
+    public function deplacer() {            // Méthode de gestion du déplacement du personnage
+         
+    }
+    
+    public function frapper() {             // Méthode de gestion de la frappe selon la force
+        
+    }
+    
+    public function gagnerExperience() {    // Methode de gestion de l'expérience du personnafe
+        
+    }
+}
