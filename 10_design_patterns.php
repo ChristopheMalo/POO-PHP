81dbd62d599605caf6ac410f73448f9fe003e82b
ChristopheMalo <christof.malo@gmail.com>
2016-02-07 17:17:34 +0100

    Ordonner les fichiers PHP

---
+++ 10_design_patterns.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <meta name="description" content="Exemples POO en PHP - Poo avancée - Les design patterns - PHP-POO OpenClassrooms et PHP Manual - Adaptation Christophe Malo">
+        <meta name="keywords" content="POO, PHP, Bootstrap, Résolution statique">
+        <meta name="author" content="Christophe Malo">
+            
+        <title>POO avancée en PHP - Les design patterns</title>
+
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <!-- Section Contenu
+        ================================================== -->
+        <div class="container">
+            <section class="row">
+                <h1 class="text-center">POO avancée en PHP - Les design patterns</h1>
+                <!-- Titre de l'exemple
+                ================================================== -->
+                <h2>Titre de l'exemple</h2>
+                <p class="col-sm-12">
+                    <?php
+                    // Code PHP
+                    ?>
+                </p>
+            </section>
+        </div>
+    </body>
+</html>
--- README.md
+++ README.md
@@ -28,3 +28,4 @@ Exemples d'utilisation de la POO - Programmation Orientée Objet - en PHP
 - Les exceptions (Lancer, Attraper, Héritage)
 - Les traits
 - UML : modélisation diagramme
+- Les design patterns
